0b9eb4f297b0a4c287dce63268e5e7b61b3969df
ksk94966 <skp.kurapati@gmail.com>
2020-05-02 02:05:06 -0500

    Have modified the method of feature extraction

--- main.py
+++ main.py
@@ -1,92 +1,103 @@
 import nltk
 import codecs
 import os
+import spacy
+nltk.download('averaged_perceptron_tagger')
+nlp = spacy.load("en_core_web_sm")
 from nltk.tokenize import word_tokenize, sent_tokenize 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 wordnet_lemmatizer = WordNetLemmatizer()
 
-doc = codecs.open("WikipediaArticles/AbrahamLincoln.txt", 'r', 'utf-8')
-content = doc.read()
 
-
-#Extacting the sentences from the text document
-sent = []
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-sent.extend(tokenizer.tokenize(content))      #sent[0] contains all the sentences
-
-print('Total Sentences : ',len(sent))
 
 #word tokenization from the sentences
-sent_wordsAll = []
-k = 0
-for i in sent:
-   sent_wordsAll.extend(nltk.word_tokenize(i))
+def sent_wordtokenization(s):
+    tok_words = nltk.word_tokenize(s)
+    print('----Word Tokenization-----')
+    print(tok_words)
 
-print('Total Words : ',len(sent_wordsAll))
 
-# punctuations="?:!.,;)("
-
-# for word in sent_wordsAll[0]:
-#     if word in punctuations:
-#         sent_wordsAll[0].remove(word)
-
-# print(len(sent_wordsAll))
-
-#lemmatizing the words
-
-word_lemm = dict()
-word_lemma_list = []
-#print("{0:20}{1:20}".format("Word","Lemma"))
-for word in sent_wordsAll:
-    word_lemm[word] = wordnet_lemmatizer.lemmatize(word)
-    #word_lemma_list.extend(wordnet_lemmatizer.lemmatize(word))
-    #print(word,word_lemm[word])
-    #print ("{0:20}{1:20}".format(word,wordnet_lemmatizer.lemmatize(word)))
-
-print('lemmacount : ',len(word_lemm))
-
-#print(len(word_lemma_list))
+#word lemmatization
+def sent_wordlemmatization(s):
+    tok_words = nltk.word_tokenize(s)
+    tempSent_lemmas = []
+    for w in tok_words:
+        tempSent_lemmas.append(wordnet_lemmatizer.lemmatize(w))
+    print('----Word Lemmatization-----')
+    print(tempSent_lemmas)
 
 #pos tagging the words with respect to each sentence
-word_tag = dict()    
-for s in sent:
-    tokenized = sent_tokenize(s)              #Transforming the sentence into one array with quotations
-    for i in tokenized: 
-        wordsList = nltk.word_tokenize(i)  
-        word_tag[s] = nltk.pos_tag(wordsList)  
+def sent_postagging(s):
+    pos_tok_templist = word_tokenize(s)
+    word_postag = nltk.pos_tag(pos_tok_templist)
+    print('----POS tagging the words-----')  
+    print(word_postag)             
+        
+def sent_wordnetFeatures(s):
+    wordslist = word_tokenize(s)
+    hypernyms_dict = dict()
+    hyponyms_dict = dict()
+    meronyms_dict = dict()
+    holonyms_dict = dict()
+    for w in wordslist:
+        temp_hyperlist = []
+        temp_hypolist = []
+        temp_merolist = []
+        temp_hololist = []
+        extracted_syns_list = wn.synsets(w)
+        if(len(extracted_syns_list)!=0):
+            for each_syn in extracted_syns_list:
+                for e in each_syn.hypernyms():
+                    for l in e.lemmas():
+                        temp_hyperlist.append(l.name())
+                for e in each_syn.hyponyms():
+                    for l in e.lemmas():
+                        temp_hypolist.append(l.name())
+                for e in each_syn.part_meronyms():
+                    for l in e.lemmas():
+                        temp_merolist.append(l.name())
+                for e in each_syn.part_holonyms():
+                    for l in e.lemmas():
+                        temp_hololist.append(l.name())
+        hypernyms_dict[w] = temp_hyperlist
+        hyponyms_dict[w] = temp_hypolist
+        meronyms_dict[w] = temp_merolist
+        holonyms_dict[w] = temp_hololist
+    print("Extracted Wordnet Features of the sentence provided")
+    print("----------Hypernyms-----------")
+    print(hypernyms_dict)
+    print("----------Hyponyms-----------")
+    print(hypernyms_dict)
+    print("----------Meronyms-----------")
+    print(hypernyms_dict)
+    print("----------Holonyms-----------")
+    print(hypernyms_dict)
 
-print(len(word_tag))
-
-#print(word_tag)
-
-hypernyms_dict = dict()
-hyponyms_dict = dict()
-meronyms_dict = dict()
-holonyms_dict = dict()
-for word in sent_wordsAll:
-    extracted_syns = wn.synsets(word)
-    #print(extracted_syns)
-    if(len(extracted_syns)!=0):
-        hypernyms_dict[word] = extracted_syns[0].hypernyms()
-        hyponyms_dict[word] = extracted_syns[0].hyponyms()
-        meronyms_dict[word]  = extracted_syns[0].part_meronyms()
-        holonyms_dict[word]  = extracted_syns[0].part_holonyms()
-
-print(len(hypernyms_dict))
-print(len(hyponyms_dict))
-print(len(meronyms_dict))
-print(len(holonyms_dict))
+def sent_parsing(s):
+    parse_sent = nlp(s)
+    for tok in parse_sent:
+        print(tok.text,"----->",tok.dep_,"----->",tok.pos_,)
 
 
 
+if __name__ == '__main__':
+	#print("Enter your input sentence:")
+    doc = codecs.open("WikipediaArticles/sample.txt", 'r', 'utf-8')
+    content = doc.read()
 
+    #Extacting the sentences from the text document
+    sent = []
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    sent.extend(tokenizer.tokenize(content))      #sent[0] contains all the sentences
 
+    print('Total Sentences After splitting the document: ',len(sent))
+    
+    print('Extracting features for each of the sentence and shown below:')
 
-
-
-
-
-
-
-
+    for s in sent:
+        sent_wordtokenization(s)
+        sent_wordlemmatization(s)
+        sent_postagging(s)
+        sent_wordnetFeatures(s)
+        sent_parsing(s)
